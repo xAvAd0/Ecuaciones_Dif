a2cc03526a060435c909a64ee6a76111b5159d04
xavie <xavie@LAPTOP-S6JS5VGG>
2024-05-26 13:08:14 -0500

    Euler

--- src/Euler/Ejemplo_2.java
+++ src/Euler/Ejemplo_2.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Euler;
 
-/**
- *
- * @author xavie
- */
+
 public class Ejemplo_2 {
     
+   
+    public static void main(String[] args) {
+        double t0 = 0;
+        double y0 = 1;
+        double h = 0.1;
+        int steps = 10;
+
+        double t = t0;
+        double y = y0;
+
+        for (int i = 0; i < steps; i++) {
+            y = y + h * f(t, y);
+            t = t + h;
+            System.out.printf("t = %.2f, y = %.5f%n", t, y);
+        }
+    }
+
+    public static double f(double t, double y) {
+        return y - t+t + 1; 
+    }
 }
+
+
